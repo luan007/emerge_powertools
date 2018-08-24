fcdab47444be951a9c1326890e6741bfc20fc02d
doge <luancfan@hotmail.com>
2018-08-24 16:08:34 +0800

    labels

--- parts/common.feed.js
+++ parts/common.feed.js
@@ -1,9 +1,67 @@
-var parse = require('parse-rss');
+(function () {
+    window.parse_rss = function (url, callback) {
+        var FeedParser, domain, options, request, rss;
+        FeedParser = require('feedparser');
+        request = require('request');
+        options = {
+            normalize: false,
+            addmeta: true,
+            feedurl: url
+        };
+        rss = [];
+        domain = require('domain').create();
+        domain.on('error', function (e) {
+            return callback(e, null);
+        });
+        return domain.run(function () {
+
+            /* Module Initialize */
+            var feedParser, req;
+            req = request(url);
+            feedParser = new FeedParser([options]);
+
+            /* REQUEST */
+            req.on('error', function (err) {
+                return callback(err, null);
+            });
+            req.on('response', function (res) {
+                var stream;
+                stream = this;
+                if (res.statusCode !== 200) {
+                    return this.emit('error', new Error('Bad status code'));
+                }
+                return stream.pipe(feedParser);
+            });
+
+            /* FEEDPARSER */
+            feedParser.on('error', function (err) {
+                return callback(err, null);
+            });
+            feedParser.on('readable', function () {
+                var item, stream;
+                stream = this;
+                if (item = stream.read()) {
+                    return rss.push(item);
+                }
+            });
+            return feedParser.on('end', function () {
+                if (rss.length === 0) {
+                    return callback('no articles');
+                }
+                return callback(null, rss, this.meta);
+            });
+        });
+    };
+})();
+
+var parse = parse_rss;
 
 var package = {
     feeds: [
         {
-            url: "https://futurism.com/search/test/feed/rss2/",
+            name: "futurism",
+            url: "https://futurism.com/feed",
+            meta: {},
             list: [],
             state: 0
         }
@@ -11,9 +69,9 @@ var package = {
 }
 
 function load() {
-    for(var i in package.feeds) {
+    for (var i in package.feeds) {
         var cur = package.feeds[i];
-        if(cur.state == 0) {
+        if (cur.state <= 0) {
             __load(cur);
         }
     }
@@ -22,10 +80,10 @@ function load() {
 function __load(pkg) {
     let url = pkg.url;
     pkg.state = 1;
-    parse(url, (err, res) => {
-        pkg.state = err ? 0 : 2;
+    parse(url, (err, res, meta) => {
+        pkg.state = err ? -1 : 2;
         pkg.list = res;
-        console.log("loaded.");
+        pkg.meta = meta;
         Vue.set(package, "feeds", package.feeds);
         vm.$forceUpdate()
     });
--- parts/tool.feed.js
+++ parts/tool.feed.js
@@ -1,20 +1,152 @@
 var router = require("./common.cardRouter");
 var feeds = require("./common.feed");
 
+var sanitizeHtml = require('sanitize-html');
+
 var cache = {};
 module.exports.data = cache;
-module.exports.name = "video";
+module.exports.name = "Feed Service";
+
+function renderFeedEntry(f) {
+    var title = (f.meta && f.meta.title) ? f.meta.title : f.name;
+    if (f.state == 0) {
+        return [{
+            bigTitle: "Pending - " + title,
+            bigTitle_ico: {
+                fas: 1,
+                "fa-circle-notch": 1,
+            },
+            path: "/feeds"
+        }];
+    } else if (f.state == -1) {
+        return [{
+            bigTitle: "Error - " + title,
+            bigTitle_ico: {
+                fas: 1,
+                "fa-exclamation-circle": 1
+            },
+            path: "/feeds"
+        }];
+    } else if (f.state == 1) {
+        return [{
+            bigTitle: "Loading - " + title,
+            bigTitle_ico: {
+                fas: 1,
+                "fa-circle-notch": 1,
+                "fa-spin": 1
+            },
+            path: "/feeds"
+        }];
+    } else if (f.state == 2) {
+        var r = [{
+            bigTitle: title + " (" + f.list.length + ")",
+            bigTitle_ico: {
+                far: 1,
+                "fa-dot-circle": 1,
+            },
+            path: "/feeds"
+        }];
+        var articles = [];
+        for (var i = 0; i < f.list.length; i++) {
+            articles = articles.concat(renderArticle(f.list[i], f));
+        }
+        return articles;
+    }
+}
+
+function renderArticle(a, f) {
+    // console.log(a);
+    var rawContent = '';
+    var lbs = [];
+    var res = {
+        src: f.meta.title + " / " + a.author,
+        src_ico: {
+            far: 1,
+            "fa-dot-circle": 1,
+        },
+        labels: lbs,
+        raw_title: a.title,
+        raw_content: rawContent,
+        onclick: () => {
+            opener(a.link);
+        }
+        // img: cache[i].thumbnail
+    };
+
+    try {
+        var vid = 0;
+        var len = 0;
+        var THRESHOLD = 300;
+        clean = sanitizeHtml(a.description, {
+            allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+                'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+                'pre', 'img', 'iframe'],
+            allowedAttributes: {
+                a: [],
+                // We don't currently allow img itself by default, but this
+                // would make sense if we did
+                img: ['src']
+            },
+            transformTags: {
+                'blockquote': 'i',
+            },
+            allowedSchemes: ['http', 'https', 'ftp'],
+            exclusiveFilter: function (frame) {
+                if (frame.tag == 'iframe') {
+                    vid = 1;
+                    return true;
+                }
+                if (len >= THRESHOLD) {
+                    return true;
+                }
+                len += frame.text.length;
+            }
+        });
+        len = 0;
+        clean = sanitizeHtml(clean, {
+            allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+                'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+                'pre', 'img'],
+            allowedAttributes: {
+                a: [],
+                // We don't currently allow img itself by default, but this
+                // would make sense if we did
+                img: ['src']
+            },
+            textFilter: function (text) {
+                len += text.length;
+                if (text.length == 0) return "";
+                var cut = len - THRESHOLD;
+                if (cut <= 0) {
+                    return text;
+                } else {
+                    return text.substring(0, text.length - cut) ? (text.substring(0, text.length - cut) + "...") : "";
+                }
+            }
+        });
+        if (vid > 0) {
+            lbs.push({
+                ico: {
+                    fas: 1,
+                    "fa-video": 1
+                },
+                text: "VID"
+            });
+        }
+        res.raw_content = clean;
+    }
+    catch (e) {
+    }
+    return [res];
+}
 
 router.on("/", (param) => {
-    return [{
-        bigTitle: "Feeds",
-        bigTitle_ico: {
-            fas: 1,
-            "fa-eye": 1
-        },
-        more: true,
-        path: "/feeds"
-    }];
+    var fd = [];
+    for (var i in feeds.package.feeds) {
+        var f = feeds.package.feeds[i];
+        fd = fd.concat(renderFeedEntry(f));
+    }
+    return fd;
 }, false, "Feed Stream");
 
 router.on("/feeds", (param) => {
